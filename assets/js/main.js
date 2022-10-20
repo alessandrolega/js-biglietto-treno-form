b71509d98f6740d620ae5b4b66d6aaa162e46468
alessandrolega <alessandrolega24@gmail.com>
2022-10-20 18:50:10 +0200

    aggiunta funzione form + link value inputs

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function GeneraPrezzo(){
+    let ValueNameSurname = document.getElementById('NameSurname').value;
+    let ValueKm = parseInt(document.getElementById('Km').value);
+    let ValueAgeUp = document.getElementById('AgeUp').value;
+    let ValueAgeDown = document.getElementById('AgeDown').value;
+    
+    
+    
+}
