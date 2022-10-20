944cf2813ed671ceb411dc1a43471d7c79020b03
alessandrolega <alessandrolega24@gmail.com>
2022-10-20 19:16:03 +0200

    aggiunti discounts + num random

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,18 @@ function GeneraPrezzo(){
     let ValueKm = parseInt(document.getElementById('Km').value);
     let ValueAgeUp = document.getElementById('AgeUp').value;
     let ValueAgeDown = document.getElementById('AgeDown').value;
+    let RandomLittle = Math.floor(Math.random() * 10) +1;
+    let RandomBig = Math.floor(Math.random() * 89999) +1;
+    
+    
+
+
+    // Discount //
+    
+    let TicketPrice = 0.21 * ValueKm;
+    let Discount20 = (TicketPrice * 0.8).toFixed(2);
+    let Discount40 = (TicketPrice * 0.6).toFixed(2);
+    
     
     
     
